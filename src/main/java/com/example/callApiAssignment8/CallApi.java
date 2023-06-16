bfbc05378aba2be203f31e742f59f29918e23e5c
Cp Maurya <cpmaurya25@gmail.com>
2023-06-16 11:11:46 +0530

    done

---
+++ src/main/java/com/example/callApiAssignment8/CallApi.java
@@ -0,0 +1,28 @@
+package com.example.callApiAssignment8;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+public class CallApi {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String url = "https://api.chucknorris.io/jokes/random";
+
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+
+        HttpResponse<String> httpResponse = httpClient.send(request,HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+
+        System.out.println(httpResponse.statusCode());
+        System.out.println(httpResponse.body());
+
+
+    }
+
+}
---
+++ src/main/java/com/example/callApiAssignment8/CallApiAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.callApiAssignment8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallApiAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallApiAssignmentApplication.class, args);
+	}
+
+}
